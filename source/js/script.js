250e7584775e83a3caaab4049be8ce1659afc432
Anastasia <anastasia.venediktova1329@gmail.com>
2023-02-16 22:36:25 +0300

    swiper

--- source/js/script.js
+++ source/js/script.js
@@ -1,4 +1,19 @@
-/* в этот файл добавляет скрипты*/
+// import Swiper, { Navigation, Pagination } from 'swiper';
+
+// Swiper.use([ Navigation, Pagination])
+
+// new Swiper('.swiper', {
+//   navigation: {
+//     nextEl: '.swiper-button-next',
+//     prevEl: '.swiper-button-prev',
+//   },
+//   pagination: {
+//     el: '.swiper-pagination',
+//     clickable: true,
+//   },
+//   mousewheel: true,
+//   keyboard: true,
+// });
 let nav = document.querySelector('.navigation');
 let navToggle = document.querySelector('.navigation__toggle');
 
@@ -13,3 +28,16 @@ navToggle.addEventListener('click', function () {
     nav.classList.remove('navigation--opened');
   }
 });
+
+const swiper = new Swiper('.swiper', {
+  navigation: {
+    nextEl: '.swiper-button-next',
+    prevEl: '.swiper-button-prev',
+  },
+  pagination: {
+    el: '.swiper-pagination',
+    type: 'bullets',
+    clickable: true,
+  },
+});
+
