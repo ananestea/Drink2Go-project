0b9961e62d2a86f46128467f29e04e2a423ea22d
Anastasia <anastasia.venediktova1329@gmail.com>
2023-02-20 06:36:47 +0300

    almost done

--- gulpfile.js
+++ gulpfile.js
@@ -1,29 +1,101 @@
 import gulp from 'gulp';
 import plumber from 'gulp-plumber';
+import gulpIf from 'gulp-if';
 import less from 'gulp-less';
 import postcss from 'gulp-postcss';
+import postUrl from 'postcss-url';
 import autoprefixer from 'autoprefixer';
+import csso from 'postcss-csso';
+import terser from 'gulp-terser';
+import squoosh from 'gulp-libsquoosh';
+import svgo from 'gulp-svgmin';
+import { stacksvg } from "gulp-stacksvg";
+import { deleteAsync } from 'del';
 import browser from 'browser-sync';
+import bemlinter from 'gulp-html-bemlinter';
+import { htmlValidator } from "gulp-w3c-html-validator";
 
-// Styles
+let isDevelopment = true;
 
-export const styles = () => {
-  return gulp.src('source/less/style.less', { sourcemaps: true })
+export function processMarkup () {
+  return gulp.src('source/*.html')
+    .pipe(gulp.dest('build'));
+}
+
+export function lintBem () {
+  return gulp.src('source/*.html')
+    .pipe(bemlinter());
+}
+
+export function validateMarkup () {
+  return gulp.src('source/*.html')
+		.pipe(htmlValidator.analyzer())
+		.pipe(htmlValidator.reporter({ throwErrors: true }));
+}
+
+export function processStyles () {
+  return gulp.src('source/less/*.less', { sourcemaps: isDevelopment })
     .pipe(plumber())
     .pipe(less())
     .pipe(postcss([
-      autoprefixer()
+      postUrl({ assetsPath: '../' }),
+      autoprefixer(),
+      csso()
     ]))
-    .pipe(gulp.dest('source/css', { sourcemaps: '.' }))
+    .pipe(gulp.dest('build/css', { sourcemaps: isDevelopment }))
     .pipe(browser.stream());
 }
 
-// Server
+export function processScripts () {
+  return gulp.src('source/js/**/*.js')
+    .pipe(terser())
+    .pipe(gulp.dest('build/js'))
+    .pipe(browser.stream());
+}
 
-const server = (done) => {
+export function optimizeImages () {
+  return gulp.src('source/img/**/*.{png,jpg}')
+    .pipe(gulpIf(!isDevelopment, squoosh()))
+    .pipe(gulp.dest('build/img'))
+}
+
+export function createWebp () {
+  return gulp.src('source/img/**/*.{png,jpg}')
+    .pipe(squoosh({
+      webp: {}
+    }))
+    .pipe(gulp.dest('build/img'))
+}
+
+export function optimizeVector () {
+  return gulp.src(['source/img/**/*.svg', '!source/img/icons/**/*.svg'])
+    .pipe(svgo())
+    .pipe(gulp.dest('build/img'));
+}
+
+export function createStack () {
+  return gulp.src('source/img/icons/**/*.svg')
+    .pipe(svgo())
+    .pipe(stacksvg())
+    .pipe(gulp.dest('build/img/icons'));
+}
+
+export function copyAssets () {
+  return gulp.src([
+    'source/fonts/**/*.{woff2,woff}',
+    'source/*.ico',
+    'source/*.webmanifest',
+    'source/leaflet/**/*',
+  ], {
+    base: 'source'
+  })
+    .pipe(gulp.dest('build'));
+}
+
+export function startServer (done) {
   browser.init({
     server: {
-      baseDir: 'source'
+      baseDir: 'build'
     },
     cors: true,
     notify: false,
@@ -32,14 +104,47 @@ const server = (done) => {
   done();
 }
 
-// Watcher
-
-const watcher = () => {
-  gulp.watch('source/less/**/*.less', gulp.series(styles));
-  gulp.watch('source/*.html').on('change', browser.reload);
+function reloadServer (done) {
+  browser.reload();
+  done();
 }
 
+function watchFiles () {
+  gulp.watch('source/less/**/*.less', gulp.series(processStyles));
+  gulp.watch('source/js/script.js', gulp.series(processScripts));
+  gulp.watch('source/*.html', gulp.series(processMarkup, reloadServer));
+}
 
-export default gulp.series(
-  styles, server, watcher
-);
+function compileProject (done) {
+  gulp.parallel(
+    processMarkup,
+    processStyles,
+    processScripts,
+    optimizeVector,
+    createStack,
+    copyAssets,
+    optimizeImages,
+    createWebp
+  )(done);
+}
+
+function deleteBuild () {
+  return deleteAsync('build');
+}
+
+export function buildProd (done) {
+  isDevelopment = false;
+  gulp.series(
+    deleteBuild,
+    compileProject
+  )(done);
+}
+
+export function runDev (done) {
+  gulp.series(
+    deleteBuild,
+    compileProject,
+    startServer,
+    watchFiles
+  )(done);
+}
